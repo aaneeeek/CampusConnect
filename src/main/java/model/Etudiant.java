a92fe0fd502cd8a43011dba7c23c8847f6fb0ce3
pierreballa97-commits <pierreballa97@gmail.com>
2026-04-28 00:04:39 +0200

    messages

--- src/main/java/model/Etudiant.java
+++ src/main/java/model/Etudiant.java
@@ -97,12 +97,12 @@ public class Etudiant extends Personne{
 	}
 	public Map VoirNote() throws SQLException {
 		Map<String, Float> Liste_note = new HashMap<>();
-		String sql = "SELECT note, intituler, nom_groupe FROM inscrire JOIN groupe ON groupe.id_groupe = inscrire.id_groupe JOIN cours ON groupe.code_cours = cours.code_cours WHERE inscrire.matricule = ?";
+		String sql = "SELECT inscrire.note, cours.code_cours, groupe.nom_groupe FROM inscrire JOIN groupe ON groupe.id_groupe = inscrire.id_groupe JOIN cours ON groupe.code_cours = cours.code_cours WHERE inscrire.matricule = ?";
 		PreparedStatement stmt = connection.prepareStatement(sql);
 		stmt.setString(1, this.matricule);
 		ResultSet rs = stmt.executeQuery();
 		while (rs.next()) {
-			Liste_note.put(rs.getString("intituler") + "("+ rs.getString("nom_groupe") + ")", rs.getFloat("note"));
+			Liste_note.put(rs.getString("code_cours") + "("+ rs.getString("nom_groupe") + ")", rs.getFloat("note"));
 		}
 		return Liste_note;
 	}
